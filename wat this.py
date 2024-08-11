c7022f2cf4569076432c2a350c07613f802f19c8
SaMuRaIchI <mongushsayin@mail.ru>
2024-08-11 21:40:30 +0700

    Add files via upload

--- wat this.py
+++ wat this.py
@@ -2,20 +2,19 @@ first = 123
 second = 456
 third = 789
 
-if first==second==third:
+if first == second == third:
     print(3)
-elif first==second and second==third:
+elif first == third or second == third or third == first:
     print(2)
 else:
     print(0)
 
-
 first = 42
 second = 69
 third = 42
-if first==second==third:
+if first == second == third:
     print(3)
-elif first==second or first==third:
+elif third == first or second == third or first == third:
     print(2)
 else:
     print(0)
